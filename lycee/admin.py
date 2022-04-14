38bc09c69228b0359464814f245fd17d86198a98
Julien LIÉGEOIS <47314333+Jliegeois23@users.noreply.github.com>
2022-04-14 09:59:37 +0000

    Début TP 04

--- lycee/admin.py
+++ lycee/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Student,Cursus
+from .models import Student, Cursus
 
 class StudentAdmin(admin.ModelAdmin):
   list_display = ("first_name", "last_name", "email", "phone")
 
-admin.site.register(Student,StudentAdmin)
-admin.site.register(Cursus)
+class CursusAdmin(admin.ModelAdmin):
+  list_display = ("scholar_year", "name", "year_from_bac")
+  fieldsets = [
+    ('nom_zone1', {'fields': ['name']}),
+    ('nom_zone2', {'fields':
+    ['scholar_year','year_from_bac'], 'classes':
+    ['collapse']}),
+  ]
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Cursus, CursusAdmin)
--- lycee/views.py
+++ lycee/views.py
@@ -2,10 +2,36 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Cursus
+from django.template import loader
+from .models import Student
 
-def index(request):
-  return HttpResponse("Racine du lycée")
+
+# def index(request):
+  # return HttpResponse("Racine du lycée")
 
 def detail(request,cursus_id):
   resp = 'result for cursus {}'.format(cursus_id)
   return HttpResponse(resp)
+
+def index (request):
+  result_list = Cursus.objects.order_by('name')
+  #chargement du template
+  '''
+  template = loader.get_template('lycee/index.html')
+  '''
+  #context
+  context = {
+    'liste' : result_list,
+  }
+  '''
+  return HttpResponse(template.render(context, request))
+  '''
+  return render (request, 'lycee/index.html', context)
+
+def detail_student(request,student_id):
+  result_list = Student.objects.get(pk=student_id)
+
+  context = {'liste' : result_list}
+
+  return render (request, 'lycee/student/detail_student.html', context)
