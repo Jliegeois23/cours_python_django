3ce1479206a0c2e8c7ecf3bd76f0ff9b9fc9145b
Julien LIÉGEOIS <47314333+Jliegeois23@users.noreply.github.com>
2022-05-19 20:57:10 +0000

    changement projet

--- lycee/admin.py
+++ lycee/admin.py
@@ -1,18 +1,24 @@
 from django.contrib import admin
+from lycee.models import Student,Cursus, Presence
+from datetime import datetime
 
-from .models import Student, Cursus
 
 class StudentAdmin(admin.ModelAdmin):
-  list_display = ("first_name", "last_name", "email", "phone")
+  list_display = ("first_name", "last_name", "email", "phone","cursus")
+  #fields = ["first_name","last_name","birth_date","email","phone","comments","cursus"]
+  fieldsets = [
+    ('main', {'fields':['first_name','last_name',"birth_date"],'classes':['collapse']}),
+    ('data', {'fields': ["email","phone","comments","cursus"],'classes':['collapse']})
+  ]
 
 class CursusAdmin(admin.ModelAdmin):
-  list_display = ("scholar_year", "name", "year_from_bac")
-  fieldsets = [
-    ('nom_zone1', {'fields': ['name']}),
-    ('nom_zone2', {'fields':
-    ['scholar_year','year_from_bac'], 'classes':
-    ['collapse']}),
-  ]
+  list_display = ("name", "scholar_year")
+
+class PresenceAdmin(admin.ModelAdmin):
+  list_display = ("date", "cursus", "isMissing", "reason")
+
 
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Cursus, CursusAdmin)
+admin.site.register(Presence, PresenceAdmin)
+
